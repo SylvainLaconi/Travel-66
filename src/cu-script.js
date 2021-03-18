ca02efb6833502586207684de90f4fa17cc37d48
Milad Behzadi <milad.bhzdi@gmail.com>
2021-03-18 10:16:48 +0100

    adding cu-script.js and edditing circuit-urban.html

---
+++ src/cu-script.js
@@ -0,0 +1,23 @@
+const carouselImages = document.querySelector('.carousel-images');
+const carouselButtons = document.querySelectorAll('.carousel-button');
+const numberOfImages = document.querySelectorAll('.carousel-images img').length;
+
+let imageIndex = 1;
+let translateX = 0;
+
+carouselButtons.forEach(button => {
+    button.addEventListener('click', (event) => {
+     if (event.target.id === 'previous') {
+        if (imageIndex !== 1) {
+          imageIndex--;
+          translateX += 500;
+        }
+      } else {
+        if (imageIndex !== numberOfImages) {
+          imageIndex++;
+          translateX -= 500;
+        }
+      }
+      carouselImages.style.transform = `translateX(${translateX}px)`;
+    });
+  });
